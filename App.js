5784d3751a905816c97537f26b232d42dba7ef4e
Hong Shik Kim <hong@hongs.dev>
2021-03-10 21:05:21 +0900

    feat: Implement notification

--- App.js
+++ App.js
@@ -1,15 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Platform } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 import reducer from './src/reducers';
 import middleware from './src/middlewares';
 import Main from './src/components/Main';
 
 const store = createStore(reducer, middleware);
 
+Notifications.setNotificationHandler({
+  handlerNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+const schedulePushNotification = async () => {
+  // const trigger = new Date(Date.now() + 5 * 1000);
+  await Notifications.cancelAllScheduledNotificationsAsync();
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Study!!",
+      body: "It's time to study with your flashcards! 📚",
+      data: { data: "Test data" },
+    },
+    // trigger,
+    trigger: { 
+      // seconds: 10,
+      hour: 10,
+      repeats: true,
+    },
+  });
+};
+
+const registerForPushNotificationsAsync = async () => {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+};
+
 export default function App() {
   console.log("### [App.render]");
+
+  const [pushToken, setPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setPushToken(token));
+    
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    schedulePushNotification();
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
 
   return (
     <Provider store={store}>
@@ -17,3 +100,8 @@ export default function App() {
     </Provider>
   );
 }
+
+
+
+
+
