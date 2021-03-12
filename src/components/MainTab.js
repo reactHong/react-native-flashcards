90d1bd4072ed137ac710df710994b1a1d8414308
Hong Shik Kim <hong@hongs.dev>
2021-03-12 15:26:52 +0900

    feat: Add icons on tab bar

--- src/components/MainTab.js
+++ src/components/MainTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import DeckListView from './DeckListView';
 import AddDeckView from './AddDeckView';
 
@@ -7,7 +8,19 @@ const Tab = createBottomTabNavigator();
 
 function MainTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Decks') {
+            iconName = focused ? 'ios-list' : 'ios-list';
+          } else if (route.name === 'Add Deck') {
+            iconName = focused ? 'add-outline' : 'add-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}>
       <Tab.Screen name="Decks" component={DeckListView} />
       <Tab.Screen name="Add Deck" component={AddDeckView} />
     </Tab.Navigator>
