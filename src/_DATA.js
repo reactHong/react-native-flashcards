c67cbc18ee67fccfeec9aeccad03f6581de0fc9c
Hong Shik Kim <hong@hongs.dev>
2021-03-12 15:52:25 +0900

    feat: Change default data

--- src/_DATA.js
+++ src/_DATA.js
@@ -21,114 +21,55 @@ export const sampleDecks = {
       }
     ]
   },
-  React1: {
-    title: 'React',
+  Redux: {
+    title: 'Redux',
     questions: [
       {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: '3 parts of Redux',
+        answer: 'store / reducer / action'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
+        question: 'In Redux, all state is stored in one location, three ways to interact with it.',
+        answer: 'getting the state / listening for changes to the state / updating the state'
       }
     ]
   },
-  JavaScript1: {
-    title: 'JavaScript',
+  Elevate: {
+    title: 'English - Elevate',
     questions: [
       {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  },
-  React2: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: 'roam',
+        answer: 'If you roam an area or roam around it, you wander or travel around it without having a particular purpose.'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript2: {
-    title: 'JavaScript',
-    questions: [
+        question: 'hush',
+        answer: "You say 'Hush!' to someone when you are asking or telling them to be quiet."
+      }, 
       {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  },
-  React3: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: 'retreat',
+        answer: 'If you retreat, you move away from something or someone.'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
+        question: 'recede',
+        answer: 'If something recedes from you, it moves away.'
+      },
     ]
   },
-  JavaScript3: {
-    title: 'JavaScript',
+  IELTS: {
+    title: 'English - IELTS',
     questions: [
       {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  },
-  React4: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: 'deplete',
+        answer: 'To deplete a stock or amount of something means to reduce it.'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript4: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  },
-  React5: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        question: 'timber',
+        answer: 'Timber is wood that is used for building houses and making furniture. You can also refer to trees that are grown for this purpose as timber.'
       },
       {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript5: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
+        question: 'prominent',
+        answer: 'Someone who is prominent is important.'
+      },
     ]
   },
 };
